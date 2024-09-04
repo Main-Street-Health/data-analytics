e661c6e72df73d12a5b39e5622f38c90e597b2f1
Brendon Pierson <brendonpierson@gmail.com>
2024-09-04 14:41:41 -0400

    Service fund one off import

--- sql/msh/20240828_med_adh_revert.sql
+++ sql/msh/20240828_med_adh_revert.sql
@@ -131,3 +131,47 @@ WHERE
 ;
 
 
+------------------------------------------------------------------------------------------------------------------------
+/* verify */
+------------------------------------------------------------------------------------------------------------------------
+SELECT
+    m.patient_id
+  , m.measure_key
+  , m.patient_measure_id
+  , pm.measure_status_key
+  , m.next_fill_date
+  , m.adr
+  , m.pdc_to_date
+  , m.measure_source_key
+  , wf.id wf_id
+  , wf.is_active
+  , wf.is_closed
+  , wf.is_reopened
+  , wf.compliance_check_date
+  , pf.id pf_id
+  , pf.drug_description
+  , pf.order_status
+  , pf.medication_status
+  , pf.pharmacy_verified_fill_date
+  , pf.pharmacy_verified_days_supply
+  , pf.system_verified_closed_at
+  , pf.inserted_at
+  , pf.updated_at
+  , pt.status
+  , pt.id
+FROM
+    qm_patient_measures pm
+    JOIN qm_pm_med_adh_metrics m ON pm.id = m.patient_measure_id
+    LEFT JOIN qm_pm_med_adh_wfs wf ON m.id = wf.qm_pm_med_adh_metric_id
+    LEFT JOIN qm_pm_med_adh_potential_fills pf ON pf.qm_pm_med_adh_wf_id = wf.id
+    LEFT JOIN patient_tasks pt ON pf.patient_task_id = pt.id
+    --     LEFT JOIN patient_medication_fills pmf ON pmf.patient_id = pm.patient_id
+--         AND pmf.measure_key = pm.measure_key
+--         AND DATE_PART('year', pmf.start_date) = pm.operational_year
+--    left join qm_pm_med_adh_synth_periods sp on pm.id = sp.patient_measure_id
+WHERE
+    pt.id in (1699618, 1658927, 1668129, 1572041, 1611246, 1089713, 1605145, 1093616, 1064800, 1078021, 1433484, 1609763,
+       1732639, 1702454, 1106093)
+    -- pt.id =
+ORDER BY pm.measure_key, pf.id
+;
--- sql/msh/20240829_med_adh_qa.sql
+++ sql/msh/20240829_med_adh_qa.sql
@@ -65,7 +65,7 @@ where
 order by id
 ;
 
-SELECT *
+SELECT *, patient_id, measure_key, pdc, adr, last_fill_date, days_supply, next_fill_date, inserted_at
 FROM
     stage.qm_pm_med_adh_mco_measures
 where
@@ -137,3 +137,185 @@ SET
     processed_at = NULL
 WHERE
     id = 8270148;
+
+------------------------------------------------------------------------------------------------------------------------
+/* https://github.com/orgs/Main-Street-Health/projects/2/views/15?filterQuery=-status%3ADone+assignee%3ABrendonPierson&pane=issue&itemId=76203345 */
+------------------------------------------------------------------------------------------------------------------------
+SELECT
+    m.patient_id
+  , m.patient_measure_id
+  , m.measure_key
+  , pm.measure_status_key
+  , m.next_fill_date
+  , m.adr
+  , m.pdc_to_date
+  , m.measure_source_key
+     , m.fill_count
+  , wf.id wf_id
+  , wf.is_active
+  , wf.is_closed
+  , wf.is_reopened
+  , wf.compliance_check_date
+  , pf.id pf_id
+  , pf.drug_description
+  , pf.order_status
+  , pf.medication_status
+  , pf.pharmacy_verified_fill_date
+  , pf.pharmacy_verified_days_supply
+  , pf.system_verified_closed_at
+  , pf.inserted_at
+  , pf.updated_at
+  , pt.status
+  , pt.id
+FROM
+    qm_patient_measures pm
+    JOIN qm_pm_med_adh_metrics m ON pm.id = m.patient_measure_id
+    LEFT JOIN qm_pm_med_adh_wfs wf ON m.id = wf.qm_pm_med_adh_metric_id
+    LEFT JOIN qm_pm_med_adh_potential_fills pf ON pf.qm_pm_med_adh_wf_id = wf.id
+    LEFT JOIN patient_tasks pt ON pf.patient_task_id = pt.id
+    --     LEFT JOIN patient_medication_fills pmf ON pmf.patient_id = pm.patient_id
+--         AND pmf.measure_key = pm.measure_key
+--         AND DATE_PART('year', pmf.start_date) = pm.operational_year
+--    left join qm_pm_med_adh_synth_periods sp on pm.id = sp.patient_measure_id
+WHERE
+    pm.patient_id = 970060
+    -- pt.id = 
+ORDER BY pm.measure_key, pf.id
+;
+SELECT *
+FROM
+    member_doc.stage.qm_pm_med_adh_mco_measures
+WHERE
+      patient_id = 970060
+  AND measure_key = 'med_adherence_cholesterol';
+
+SELECT *
+FROM
+    patient_medication_fills
+WHERE
+      patient_id = 970060
+  AND measure_key = 'med_adherence_cholesterol';
+
+------------------------------------------------------------------------------------------------------------------------
+/* https://github.com/orgs/Main-Street-Health/projects/2/views/15?filterQuery=-status%3ADone+assignee%3ABrendonPierson&pane=issue&itemId=76203968 */
+------------------------------------------------------------------------------------------------------------------------
+SELECT
+    m.patient_id
+  , m.patient_measure_id
+  , m.measure_key
+  , pm.measure_status_key
+  , m.next_fill_date
+  , m.adr
+  , m.pdc_to_date
+  , m.measure_source_key
+  , wf.id wf_id
+  , wf.is_active
+  , wf.is_closed
+  , wf.is_reopened
+  , wf.compliance_check_date
+  , pf.id pf_id
+  , pf.drug_description
+  , pf.order_status
+  , pf.medication_status
+  , pf.pharmacy_verified_fill_date
+  , pf.pharmacy_verified_days_supply
+  , pf.system_verified_closed_at
+  , pf.inserted_at
+  , pf.updated_at
+  , pt.status
+  , pt.id
+FROM
+    qm_patient_measures pm
+    JOIN qm_pm_med_adh_metrics m ON pm.id = m.patient_measure_id
+    LEFT JOIN qm_pm_med_adh_wfs wf ON m.id = wf.qm_pm_med_adh_metric_id
+    LEFT JOIN qm_pm_med_adh_potential_fills pf ON pf.qm_pm_med_adh_wf_id = wf.id
+    LEFT JOIN patient_tasks pt ON pf.patient_task_id = pt.id
+    --     LEFT JOIN patient_medication_fills pmf ON pmf.patient_id = pm.patient_id
+--         AND pmf.measure_key = pm.measure_key
+--         AND DATE_PART('year', pmf.start_date) = pm.operational_year
+--    left join qm_pm_med_adh_synth_periods sp on pm.id = sp.patient_measure_id
+WHERE
+    pm.patient_id = 626115
+    -- pt.id = 
+ORDER BY pm.measure_key, pf.id
+;
+
+SELECT *
+FROM
+    member_doc.stage.qm_pm_med_adh_mco_measures
+WHERE
+      patient_id = 626115
+  AND measure_key = 'med_adherence_cholesterol'
+;
+
+SELECT *
+FROM
+    qm_pm_med_adh_handoffs
+WHERE
+      patient_id = 626115
+  AND measure_key = 'med_adherence_cholesterol'
+order by id
+;
+
+
+------------------------------------------------------------------------------------------------------------------------
+/* https://github.com/orgs/Main-Street-Health/projects/2/views/15?filterQuery=-status%3ADone+assignee%3ABrendonPierson&pane=issue&itemId=76215081
+ 1347423 */
+------------------------------------------------------------------------------------------------------------------------
+SELECT
+    m.patient_id
+  , m.measure_key
+  , m.patient_measure_id
+  , pm.measure_status_key
+  , m.next_fill_date
+  , m.adr
+  , m.pdc_to_date
+  , m.measure_source_key
+  , wf.id wf_id
+  , wf.is_active
+  , wf.is_closed
+  , wf.is_reopened
+  , wf.compliance_check_date
+  , pf.id pf_id
+  , pf.drug_description
+  , pf.order_status
+  , pf.medication_status
+  , pf.pharmacy_verified_fill_date
+  , pf.pharmacy_verified_days_supply
+  , pf.system_verified_closed_at
+  , pf.inserted_at
+  , pf.updated_at
+  , pt.status
+  , pt.id
+FROM
+    qm_patient_measures pm
+    JOIN qm_pm_med_adh_metrics m ON pm.id = m.patient_measure_id
+    LEFT JOIN qm_pm_med_adh_wfs wf ON m.id = wf.qm_pm_med_adh_metric_id
+    LEFT JOIN qm_pm_med_adh_potential_fills pf ON pf.qm_pm_med_adh_wf_id = wf.id
+    LEFT JOIN patient_tasks pt ON pf.patient_task_id = pt.id
+    --     LEFT JOIN patient_medication_fills pmf ON pmf.patient_id = pm.patient_id
+--         AND pmf.measure_key = pm.measure_key
+--         AND DATE_PART('year', pmf.start_date) = pm.operational_year
+--    left join qm_pm_med_adh_synth_periods sp on pm.id = sp.patient_measure_id
+WHERE
+    pm.patient_id = 1347423
+    -- pt.id =
+ORDER BY pm.measure_key, pf.id
+;
+SELECT *
+FROM
+    patient_medication_fills
+WHERE
+    patient_id = 1347423
+and drug_description ~* 'entresto'
+;
+
+-- SELECT *
+-- FROM
+--     analytics.ref.med_adherence_value_sets vs
+--     JOIN ref.med_adherence_measures m ON vs.value_set_id = m.value_set_id
+-- WHERE
+--       code = '00078065920'
+--   AND m.measure_id = 'PDC-RASA'
+-- and m.measure_version = '2024'
+    ;
